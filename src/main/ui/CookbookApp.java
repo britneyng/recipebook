39c868cdee16729f4a1b6ef6dfdcdde50d52c9c3
Britney Ng <britneyng1@gmail.com>
2022-02-24 16:52:33 -0800

    add functionality to store multiple recipes

--- src/main/ui/CookbookApp.java
+++ src/main/ui/CookbookApp.java
@@ -8,12 +8,11 @@ import java.util.Scanner;
 public class CookbookApp {
 
     private Scanner input;
-    private Cookbook cookbook;
+    private Cookbook cookbook = new Cookbook();
     private IngredientList ingredientList;
     private StepList stepList;
     private Ingredient ingredient;
     private Step step;
-    private Recipe recipe;
 
     // EFFECTS: runs the Cookbook app
     public CookbookApp() {
@@ -51,7 +50,7 @@ public class CookbookApp {
     // EFFECTS: processes user command
     private void processCommand(String command) {
         if (command.equals("v")) {
-            viewRecipes();
+            viewAllRecipes();
         } else if (command.equals("w")) {
             createRecipe();
         } else if (command.equals("r")) {
@@ -64,7 +63,7 @@ public class CookbookApp {
     // EFFECTS: displays menu of options to user
     private void displayMenu() {
         System.out.println("\nSelect from:");
-        System.out.println("\tv -> view recipes");
+        System.out.println("\tv -> view list of added recipes");
         System.out.println("\tw -> write a recipe");
         System.out.println("\tr -> remove recipe");
         System.out.println("\tq -> quit");
@@ -77,15 +76,13 @@ public class CookbookApp {
 
         createIngredientList(ingredientList);
         writeStepList(stepList);
-        this.recipe = new Recipe(recipeName, ingredientList, stepList);
-        this.cookbook = new Cookbook();
-        cookbook.addRecipe(this.recipe);
+        cookbook.addRecipe(new Recipe(recipeName, ingredientList, stepList));
         System.out.println("The recipe has been added to the Cookbook :)");
 
     }
 
     // EFFECTS: creates an ingredient list for the recipe
-    private void createIngredientList(IngredientList ingredientList) {
+    private void createIngredientList(IngredientList ingredients) {
         System.out.println("Let's add some ingredients!");
         System.out.println("How many ingredients are in this recipe?");
         int quantity = input.nextInt();
@@ -99,9 +96,9 @@ public class CookbookApp {
             String ingredientUnit = input.next();
 
             ingredient =  new Ingredient(ingredientName, ingredientAmount, ingredientUnit);
-            ingredientList = new IngredientList();
-            ingredientList.addIngredient(ingredient);
-            ingredientList.getIngredientList();
+            ingredients = new IngredientList();
+            ingredients.addIngredient(ingredient);
+            ingredients.getIngredientList();
 
         }
     }
@@ -123,29 +120,30 @@ public class CookbookApp {
         }
     }
 
+    // EFFECTS: lists recipes to be viewed by the user
+    public void viewAllRecipes() {
+        listRecipes();
+
+    }
+
     // EFFECTS: removes recipe from Cookbook if input matches an existing recipe name
     public void removeRecipe() {
         System.out.println("Which recipe would you like to remove? Enter the name: ");
         listRecipes();
         String recipeToBeRemoved = input.next();
         cookbook.removeRecipe(cookbook.findRecipe(recipeToBeRemoved));
-
-
-    }
-
-    // EFFECTS: lists recipes to be viewed by the user
-    public void viewRecipes() {
-        listRecipes();
-
     }
 
     // EFFECTS: return a list of added recipes in the Cookbook
     public void listRecipes() {
         for (Recipe r : cookbook.getRecipeList()) {
-            System.out.println(recipe.getRecipeTitle());
+            System.out.println(r.getRecipeTitle());
         }
-
     }
 
+    // EFFECTS: view a recipe in detail with a list of ingredients and stepse
+//    public void viewDetailedRecipe(){
+//
+//    }
 }
 
